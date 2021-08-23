e6d7b1d2f7503863dedc082aa91f8b7d826df11a
sash <na-okne@mail.ru>
2021-08-24 02:03:31 +0300

    day4

---
+++ js/script.js
@@ -0,0 +1,26 @@
+new Choices('#model', {
+	searchEnabled: false,
+	itemSelectText: '',
+})
+
+new Choices('#color', {
+	searchEnabled: false,
+	itemSelectText: '',
+})
+
+$('#form-order').on('submit', function(event) {
+	event.preventDefault();
+	$.ajax({
+		url: 'https://jsonplaceholder.typicode.com/posts',
+		method: 'POST',
+		dataType: 'html',
+		data: $('#form-order').serialize(),
+		success: function(data) {
+			console.log(data);
+			$('#form-order').html('Спасибо, ваша заявка успешно отправлена');
+			$('.fotm__submit').hide()
+
+		}
+	})
+})
+
